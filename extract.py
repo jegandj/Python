9044df1b1faf799ff762720397ad3f269a8a18de
jegandj <65338148+jegandj@users.noreply.github.com>
2021-06-10 08:09:32 +0100

    Add files via upload

---
+++ extract.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 14:35:44 2020
+
+@author: DaviesJones
+"""
+
+import os
+import pandas as pd
+os.chdir('C://Users//DaviesJones//Documents//Python Scripts//Start')
+df = pd.read_excel("WRSX.xlsx")
